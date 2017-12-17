2badf0a728b5b76512f92aa1dc32556d83790f75
Ubuntu <tom@DeepLearningVM.po3eliejoune5ne11dzkyu2ivh.bx.internal.cloudapp.net>
2017-12-17 23:54:08 +0000

    Fully scaling up the FC layers for my VGGLite

--- tom_train_1.py
+++ tom_train_1.py
@@ -190,8 +190,8 @@ saver = tf.train.Saver(tf.global_variables())
 tf.summary.scalar("cross_entropy",loss_mean)
 tf.summary.scalar("accuracy",accuracy_tensor)
 merged_summaries = tf.summary.merge_all()
-train_writer = tf.summary.FileWriter("logs/train_unknown_mfcc_vggnetlite_biggerFC",sess.graph)
-val_writer = tf.summary.FileWriter("logs/val_unknown_mfcc_vggnetlite_biggerFC",sess.graph)
+train_writer = tf.summary.FileWriter("logs/train_unknown_mfcc_vggnetlite_biggestFC",sess.graph)
+val_writer = tf.summary.FileWriter("logs/val_unknown_mfcc_vggnetlite_biggestFC",sess.graph)
 
 
 tf.logging.set_verbosity(tf.logging.INFO)
