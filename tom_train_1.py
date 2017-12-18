e016f27f9293227560645ebc3e805dcfd58ceefa
Ubuntu <tom@DeepLearningVM.po3eliejoune5ne11dzkyu2ivh.bx.internal.cloudapp.net>
2017-12-18 14:59:46 +0000

    Trying my first 1d conv

--- tom_train_1.py
+++ tom_train_1.py
@@ -170,10 +170,10 @@ wav_ph = tf.placeholder(tf.float32,(None,sample_rate))
 keep_prob = tf.placeholder(tf.float32) # will be 0.5 for training, 1 for test
 learning_rate_ph = tf.placeholder(tf.float32,[],name="learning_rate_ph")
 
-features = make_features(wav_ph,"mfcc")
+features = make_features(wav_ph,"identity")
 
 output_neurons = len(all_words) if style == "full" else len(wanted_words)
-final_layer = vggnet(features,keep_prob,output_neurons)
+final_layer = oned_conv(features,keep_prob,output_neurons)
 
 loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels_ph, logits=final_layer)
 loss_mean = tf.reduce_mean(loss)
@@ -190,8 +190,8 @@ saver = tf.train.Saver(tf.global_variables())
 tf.summary.scalar("cross_entropy",loss_mean)
 tf.summary.scalar("accuracy",accuracy_tensor)
 merged_summaries = tf.summary.merge_all()
-train_writer = tf.summary.FileWriter("logs/train_unknown_mfcc_vggnetlite_biggestFC_changingdropout",sess.graph)
-val_writer = tf.summary.FileWriter("logs/val_unknown_mfcc_vggnetlite_biggestFC_90_changingdropout",sess.graph)
+train_writer = tf.summary.FileWriter("logs/train_unknown_1dconv",sess.graph)
+val_writer = tf.summary.FileWriter("logs/val_unknown_1dconv",sess.graph)
 
 
 tf.logging.set_verbosity(tf.logging.INFO)
@@ -199,8 +199,6 @@ sess.run(tf.global_variables_initializer())
 saver = tf.train.Saver()
 last_val_accuracy = 0
 for i in range(steps):
-    # if i > 0 and i % decay_every == 0:
-    #     learning_rate = learning_rate * decay_rate
     feed_dict = get_batch(data_index["train"],batch_size,style=style)
     feed_dict.update({keep_prob: 0.5,learning_rate_ph:learning_rate})
     # now here's where we run the real, convnet part
@@ -231,13 +229,10 @@ for i in range(steps):
         df_words = all_words if style == "full" else wanted_words
         pd.DataFrame(val_conf_mat,columns=df_words,index=df_words).to_csv("confusion_matrix_{}.csv".format(i))
 
-        if val_acc.calculate() < last_val_accuracy:
+        if val_acc.calculate() < last_val_accuracy and i > 500:
             learning_rate = decay_rate*learning_rate
             print("CHANGING LEARNING RATE TO: {}".format(learning_rate))
-            saver.restore(sess,"model.ckpt")
-        else: # now only update if accuracy is higher than last val
-            saver.save(sess,"model.ckpt")
-            last_val_accuracy = val_acc.calculate()
+        last_val_accuracy = val_acc.calculate()
 
     if learning_rate < 0.00001: # at this point, just stop
         break
