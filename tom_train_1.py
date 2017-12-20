57b8413c8903265b3b676ef1712c7e489d4973c5
Tom <omalleyt@bu.edu>
2017-12-20 17:55:00 +0000

    Changing TB name

--- tom_train_1.py
+++ tom_train_1.py
@@ -178,8 +178,8 @@ saver = tf.train.Saver(tf.global_variables())
 tf.summary.scalar("cross_entropy",loss_mean)
 tf.summary.scalar("accuracy",accuracy_tensor)
 merged_summaries = tf.summary.merge_all()
-train_writer = tf.summary.FileWriter("logs/train_unknown_driveconv1_2",sess.graph)
-val_writer = tf.summary.FileWriter("logs/val_unknown_driveconv1_2",sess.graph)
+train_writer = tf.summary.FileWriter("logs/train_unknown_driveconv1_3",sess.graph)
+val_writer = tf.summary.FileWriter("logs/val_unknown_driveconv1_3",sess.graph)
 
 
 tf.logging.set_verbosity(tf.logging.INFO)
