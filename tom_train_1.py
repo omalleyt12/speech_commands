42ff060ca9cd9827612963142636b96aff2f09c6
Tom <omalleyt@bu.edu>
2017-12-21 22:14:59 +0000

    Removed speedx and changed logs

--- tom_train_1.py
+++ tom_train_1.py
@@ -183,8 +183,8 @@ saver = tf.train.Saver(tf.global_variables())
 tf.summary.scalar("cross_entropy",loss_mean)
 tf.summary.scalar("accuracy",accuracy_tensor)
 merged_summaries = tf.summary.merge_all()
-train_writer = tf.summary.FileWriter("logs/train_unknown_driveconv1_tom1d2",sess.graph)
-val_writer = tf.summary.FileWriter("logs/val_unknown_driveconv1_tom1d2",sess.graph)
+train_writer = tf.summary.FileWriter("logs/train_unknown_driveconv1_good_distortions",sess.graph)
+val_writer = tf.summary.FileWriter("logs/val_unknown_driveconv1_good_distortions",sess.graph)
 
 
 tf.logging.set_verbosity(tf.logging.INFO)
