4ba254019c230882a28638e6078ec42671033ed9
Tom <omalleyt@bu.edu>
2017-12-26 22:31:21 +0000

    The surprisingly good model Overdrive

--- my_models.py
+++ my_models.py
@@ -73,52 +73,39 @@ def vggnet(features,keep_prob,num_final_neurons):
 
     return final_layer
 
-# This is my best model yet, with LB of 0.86
+# I'm now using this as the Ferrari version of what I just tried
 def drive_conv_log_mel(features,keep_prob,num_final_neurons,is_training):
     """Let's assume we have a 128 bin spectrogram here"""
     fingerprint_4d = tf.reshape(features,[-1,features.shape[1],features.shape[2],1])
 
-    c = tf.contrib.layers.conv2d(fingerprint_4d,64,[3,3],[1,1])
-    mp = tf.nn.max_pool(c,[1,2,2,1],[1,2,2,1],"VALID")
-    c = tf.contrib.layers.conv2d(mp,128,[3,3],[1,1])
-    mp = tf.nn.max_pool(c,[1,2,2,1],[1,2,2,1],"VALID")
-    c = tf.contrib.layers.conv2d(mp,128,[3,3],[1,1])
-    mp = tf.nn.max_pool(c,[1,1,2,1],[1,1,2,1],"VALID")
-
-    print(mp.shape)
-    for channels in [192,256,384,512]:
-        c = tf.contrib.layers.conv2d(mp,channels,[1,3],[1,1])
-        mp = tf.nn.max_pool(c,[1,1,2,1],[1,1,2,1],"VALID")
-    c = tf.contrib.layers.conv2d(mp,256,[1,1],[1,1])
-
+    c = conv2d(fingerprint_4d,128,[7,7],is_training,mp=[1,3])
+    c = conv2d(c,192,[1,7],is_training,mp=[1,2])
+    c = conv2d(c,256,[1,7],is_training,mp=[1,2])
     print(c.shape)
 
+    c = conv2d(c,512,[1,c.shape[2]],is_training,padding="VALID")
+    c = conv2d(c,1024,[1,1],is_training,mp=[c.shape[1],1])
+    c = tf.contrib.layers.flatten(c)
+    c = tf.nn.dropout(c,keep_prob)
+    print(c.shape)
 
-    c7_3 = tf.contrib.layers.conv2d(c,128,[3,1],[1,1],"VALID")
-    c7_5 = tf.contrib.layers.conv2d(c,128,[5,1],[1,1],"VALID")
-    c7_10 = tf.contrib.layers.conv2d(c,128,[10,1],[1,1],"VALID")
+    fc1 = tf.contrib.layers.fully_connected(c,2048)
+    fc1 = tf.nn.dropout(fc1,keep_prob)
 
-    conv_out = tf.concat([
-        tf.contrib.layers.flatten(c7_3),
-        tf.contrib.layers.flatten(c7_5),
-        tf.contrib.layers.flatten(c7_10)
-    ],axis=1)
+    fc2 = tf.contrib.layers.fully_connected(fc1,1024)
+    fc2 = tf.nn.dropout(fc2,keep_prob)
 
-    print(conv_out.shape)
+    final_layer = tf.contrib.layers.fully_connected(fc2,num_final_neurons)
 
-    dropout_conv = tf.nn.dropout(conv_out,keep_prob)
-
-    fc1 = tf.contrib.layers.fully_connected(dropout_conv,3000)
-    dropout_fc1 = tf.nn.dropout(fc1,keep_prob)
-
-    fc2 = tf.contrib.layers.fully_connected(dropout_fc1,1500)
-    dropout_fc2 = tf.nn.dropout(fc2,keep_prob)
-
-    final_layer = tf.contrib.layers.fully_connected(dropout_fc2,num_final_neurons,activation_fn=None)
+    print(final_layer.shape)
 
     return final_layer
 
-def conv2d(x,channels,kernel_size,is_training,strides=[1,1],padding="SAME",mp=None,bn=True):
+
+
+
+
+def conv2d(x,channels,kernel_size,is_training,strides=[1,1],padding="SAME",mp=None,bn=False):
     """Make sure to update training ops when using this, can run something like:
 
     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
@@ -135,9 +122,51 @@ def conv2d(x,channels,kernel_size,is_training,strides=[1,1],padding="SAME",mp=No
     else:
         return c
 
-def overdrive(features,keep_prob,num_final_neurons,is_training):
-    print("hey")
+def spec2phone(features,is_training,type=None):
+    """These are the various strategies for going from 128 Log Mel Spectrogram => Phone"""
+    fingerprint_4d = tf.reshape(features,[-1,features.shape[1],features.shape[2],1])
+    if type == "drive_conv": # not "fixed"
+        c = conv2d(fingerprint_4d,64,[3,3],is_training,mp=[2,2])
+        c = conv2d(c,128,[3,3],is_training,mp=[2,2])
+        c = conv2d(c,128,[3,3],is_training,mp=[1,2])
 
+        for channels in [192,256,384,512]:
+            c = conv2d(c,channels,[3,1],is_training,mp=[1,2])
+        c = conv2d(c,256,[1,1],is_training)
+
+        print(c.shape)
+    elif type == "overdrive":
+        c = conv2d(fingerprint_4d,64,[7,7],is_training,mp=[1,5])
+        c = conv2d(c,128,[1,3],is_training,mp=[1,2])
+        print(c.shape)
+
+        c = conv2d(c,256,[1,c.shape[2]],is_training,padding="VALID")
+        c = conv2d(c,512,[3,1],is_training,mp=[3,1])
+        print(c.shape)
+
+
+def parts_conv(features,keep_prob,num_final_neurons,is_training):
+    c = spec2phone(features,is_training,"overdrive")
+
+def overdrive(features,keep_prob,num_final_neurons,is_training):
+    fingerprint_4d = tf.reshape(features,[-1,features.shape[1],features.shape[2],1])
+
+    c = conv2d(fingerprint_4d,64,[7,3],is_training,mp=[1,3])
+    c = conv2d(c,128,[1,7],is_training,mp=[1,4])
+
+    c = conv2d(c,256,[1,10],is_training,padding="VALID",mp=[98,1])
+
+    c = tf.contrib.layers.flatten(c)
+
+    d = tf.nn.dropout(c,keep_prob)
+    fc = tf.contrib.layers.fully_connected(d,50)
+
+    d2 = tf.nn.dropout(fc,keep_prob)
+
+    final_layer = tf.contrib.layers.fully_connected(d2,num_final_neurons)
+    print(c.shape)
+    print(d.shape)
+    return final_layer
 
 def ttagau_conv(features,keep_prob,num_final_neurons,is_training):
     x = tf.reshape(features,[-1,features.shape[1],1,1])
