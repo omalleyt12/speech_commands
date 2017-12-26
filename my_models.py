777cb0e870870630d83e8c8987adeb12c62e47fd
Tom <omalleyt@bu.edu>
2017-12-26 19:24:15 +0000

    LR is 0.3

--- my_models.py
+++ my_models.py
@@ -1,75 +1,6 @@
 import tensorflow as tf
 from keras.layers import GlobalMaxPool2D
 
-def orig_conv(features,keep_prob,num_final_neurons):
-    """The original model by the Google guy, works for a 2d speech image feature (MFCC or Log Mel)"""
-    fingerprint_4d = tf.reshape(features,[-1,features.shape[1],features.shape[2],1])
-
-    conv_1_channels = 64
-    conv_2_channels = 64
-
-    weights_1 = tf.Variable(tf.truncated_normal([20,8,1,conv_1_channels],stddev=0.01)) # [height,width,depth,channels]
-    bias_1 = tf.Variable(tf.zeros([64]))
-    conv_1 = tf.nn.conv2d(fingerprint_4d,weights_1,[1,1,1,1],"SAME") + bias_1
-    relu_1 = tf.nn.relu(conv_1)
-    dropout_1 = tf.nn.dropout(relu_1,keep_prob)
-    maxpool_1 = tf.nn.max_pool(dropout_1,[1,2,2,1],[1,2,2,1],"SAME")
-
-    weights_2 = tf.Variable(tf.truncated_normal([10,4,conv_1_channels,conv_2_channels],stddev=0.01)) # [height,width,first_conv_channels,second_conv_channels]
-    bias_2 = tf.Variable(tf.zeros([64]))
-    conv_2 = tf.nn.conv2d(maxpool_1,weights_2,[1,1,1,1],"SAME") + bias_2
-    relu_2 = tf.nn.relu(conv_2)
-    dropout_2 = tf.nn.dropout(relu_2,keep_prob)
-
-    _ , now_height, now_width, _ = dropout_2.get_shape()
-    now_height = int(now_height)
-    now_width = int(now_width)
-    now_flat_elements = now_height * now_width * conv_2_channels
-
-    flat_layer = tf.reshape(dropout_2,[-1,now_flat_elements])
-
-    weights_3 = tf.Variable(tf.truncated_normal([now_flat_elements,num_final_neurons],stddev=0.01))
-    bias_3 = tf.Variable(tf.zeros(num_final_neurons))
-    final_layer = tf.matmul(flat_layer,weights_3) + bias_3
-    return final_layer
-
-def orig_with_extra_fc(features,keep_prob,num_final_neurons):
-    fingerprint_4d = tf.reshape(features,[-1,features.shape[1],features.shape[2],1])
-
-    conv_1_channels = 64
-    conv_2_channels = 64
-
-    weights_1 = tf.Variable(tf.truncated_normal([20,8,1,conv_1_channels],stddev=0.01)) # [height,width,depth,channels]
-    bias_1 = tf.Variable(tf.zeros([64]))
-    conv_1 = tf.nn.conv2d(fingerprint_4d,weights_1,[1,1,1,1],"SAME") + bias_1
-    relu_1 = tf.nn.relu(conv_1)
-    dropout_1 = tf.nn.dropout(relu_1,keep_prob)
-    maxpool_1 = tf.nn.max_pool(dropout_1,[1,2,2,1],[1,2,2,1],"SAME")
-
-    weights_2 = tf.Variable(tf.truncated_normal([10,4,conv_1_channels,conv_2_channels],stddev=0.01)) # [height,width,first_conv_channels,second_conv_channels]
-    bias_2 = tf.Variable(tf.zeros([64]))
-    conv_2 = tf.nn.conv2d(maxpool_1,weights_2,[1,1,1,1],"SAME") + bias_2
-    relu_2 = tf.nn.relu(conv_2)
-    dropout_2 = tf.nn.dropout(relu_2,keep_prob)
-
-    _ , now_height, now_width, _ = dropout_2.get_shape()
-    now_height = int(now_height)
-    now_width = int(now_width)
-    now_flat_elements = now_height * now_width * conv_2_channels
-
-    flat_layer = tf.reshape(dropout_2,[-1,now_flat_elements])
-    print("Flat Layer 1 shape {}".format(now_flat_elements))
-
-    weights_3 = tf.Variable(tf.truncated_normal([now_flat_elements,2048],stddev=0.01))
-    bias_3 = tf.Variable(tf.zeros(2048))
-    flat_layer_2 = tf.nn.relu(tf.matmul(flat_layer,weights_3) + bias_3)
-    dropout_3 = tf.nn.dropout(flat_layer_2,keep_prob)
-
-    weights_4 = tf.Variable(tf.truncated_normal([2048,num_final_neurons],stddev=0.01))
-    bias_4 = tf.Variable(tf.zeros(num_final_neurons))
-    final_layer = tf.matmul(dropout_3,weights_4) + bias_4
-    return final_layer
-
 def vggnet(features,keep_prob,num_final_neurons):
     fingerprint_4d = tf.reshape(features,[-1,features.shape[1],features.shape[2],1])
 
@@ -142,44 +73,6 @@ def vggnet(features,keep_prob,num_final_neurons):
 
     return final_layer
 
-def drive_conv(features,keep_prob,num_final_neurons):
-    fingerprint_4d = tf.reshape(features,[-1,features.shape[1],features.shape[2],1])
-
-    c1 = tf.contrib.layers.conv2d(fingerprint_4d,64,[3,3],[1,1])
-    c2 = tf.contrib.layers.conv2d(c1,128,[1,3],[1,1])
-    mp1 = tf.nn.max_pool(c2,[1,1,2,1],[1,1,2,1],"SAME")
-    c3 = tf.contrib.layers.conv2d(mp1,256,[1,5],[1,1])
-    mp2 = tf.nn.max_pool(c3,[1,1,2,1],[1,1,2,1],"SAME")
- 
-    c4 = tf.contrib.layers.conv2d(mp2,512,[1,10],[1,1],"VALID")
-
-    c5 = tf.contrib.layers.conv2d(c4,512,[3,1],[1,1],"VALID")
-    c6 = tf.contrib.layers.conv2d(c5,256,[1,1],[1,1])
-    mp3 = tf.nn.max_pool(c6,[1,3,1,1],[1,3,1,1],"SAME")
-    print(mp3.shape)
-
-    c7_3 = tf.contrib.layers.conv2d(mp3,128,[3,1],[1,1],"VALID")
-    c7_5 = tf.contrib.layers.conv2d(mp3,128,[5,1],[1,1],"VALID")
-    c7_10 = tf.contrib.layers.conv2d(mp3,128,[10,1],[1,1],"VALID")
-
-    conv_out = tf.concat([
-        tf.contrib.layers.flatten(c7_3),
-        tf.contrib.layers.flatten(c7_5),
-        tf.contrib.layers.flatten(c7_10)
-    ],axis=1)
-
-    dropout_conv = tf.nn.dropout(conv_out,keep_prob)
-
-    fc1 = tf.contrib.layers.fully_connected(dropout_conv,3000)
-    dropout_fc1 = tf.nn.dropout(fc1,keep_prob)
-
-    fc2 = tf.contrib.layers.fully_connected(dropout_fc1,1500)
-    dropout_fc2 = tf.nn.dropout(fc2,keep_prob)
-
-    final_layer = tf.contrib.layers.fully_connected(dropout_fc2,num_final_neurons,activation_fn=None)
-
-    return final_layer
-
 # This is my best model yet, with LB of 0.86
 def drive_conv_log_mel(features,keep_prob,num_final_neurons,is_training):
     """Let's assume we have a 128 bin spectrogram here"""
@@ -241,6 +134,9 @@ def conv2d(x,channels,kernel_size,is_training,strides=[1,1],padding="SAME",mp=No
         return tf.nn.max_pool(c,[1,mp[0],mp[1],1],[1,mp[0],mp[1],1],"VALID")
     else:
         return c
+
+def overdrive(features,keep_prob,num_final_neurons,is_training):
+    print("hey")
 
 
 def ttagau_conv(features,keep_prob,num_final_neurons,is_training):
