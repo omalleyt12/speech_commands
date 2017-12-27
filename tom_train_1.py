7f536c2d815f2539d68574d8cecf741eb376719a
Tom <omalleyt@bu.edu>
2017-12-27 05:35:16 +0000

    Adding a second FC layer to Overdrive

--- tom_train_1.py
+++ tom_train_1.py
@@ -242,8 +242,8 @@ saver = tf.train.Saver(tf.global_variables())
 tf.summary.scalar("cross_entropy",loss_mean)
 tf.summary.scalar("accuracy",accuracy_tensor)
 merged_summaries = tf.summary.merge_all()
-train_writer = tf.summary.FileWriter("logs/train_unknown_overdrive_v2",sess.graph)
-val_writer = tf.summary.FileWriter("logs/val_unknown_overdrive_v2",sess.graph)
+train_writer = tf.summary.FileWriter("logs/train_unknown_overdrive_v3",sess.graph)
+val_writer = tf.summary.FileWriter("logs/val_unknown_overdrive_v3",sess.graph)
 
 
 tf.logging.set_verbosity(tf.logging.INFO)
