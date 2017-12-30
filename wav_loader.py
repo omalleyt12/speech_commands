fd61455e57e8f60c951c03b9f84eb1ae66ee0025
Thomas O'Malley <tom.omalley@chubb.com>
2017-12-30 12:01:02 -0500

    Changing background strategy

--- wav_loader.py
+++ wav_loader.py
@@ -25,7 +25,7 @@ def load_test_data(sess):
     test_dir = "test/audio"
     test_index = []
     for i,wav_path in enumerate(gfile.Glob("test/audio/*.wav")):
-        if i % 1000 == 0: print("Test {}".format(i))
+        if i % 10000 == 0: print("Test {}".format(i))
         tdata = wav_loader.load(wav_path,sess)
         file_name = os.path.basename(wav_path)
         test_index.append({"file":wav_path,"identifier":file_name,"data":tdata})
