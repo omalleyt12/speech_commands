bef0ca65d226628a75598f9c3a2eb2cdf2fd0622
Tom <omalleyt@bu.edu>
2017-12-31 02:52:38 +0000

    Running Overdrive BN Full again with appropriately loud noise

--- tom_train_1.py
+++ tom_train_1.py
@@ -245,8 +245,8 @@ saver = tf.train.Saver(tf.global_variables())
 tf.summary.scalar("cross_entropy",loss_mean)
 tf.summary.scalar("accuracy",accuracy_tensor)
 merged_summaries = tf.summary.merge_all()
-train_writer = tf.summary.FileWriter("logs/train_unknown_overdrive_full_bn_new_noise",sess.graph)
-val_writer = tf.summary.FileWriter("logs/val_unknown_overdrive_full_bn_new_noise",sess.graph)
+train_writer = tf.summary.FileWriter("logs/train_unknown_overdrive_full_bn_loud_noise",sess.graph)
+val_writer = tf.summary.FileWriter("logs/val_unknown_overdrive_full_bn_loud_noise",sess.graph)
 
 
 tf.logging.set_verbosity(tf.logging.INFO)
