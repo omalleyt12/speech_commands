24a8e2466ab622be1ee4ab1e8fcb9184e02da822
Ubuntu <tom@DeepLearning2.i5ezdeopbsrunjoorlupdwlw1g.cx.internal.cloudapp.net>
2018-01-14 06:18:14 +0000

    f

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python tom_train_1.py --features long-log-mel --model overdrive --save overdrive_long --train --no-val
-python tom_train_1.py --features long-log-mel --model overdrive --save overdrive_long_pl1 --pseudo_labels overdrive_long --train --no-val
-python tom_train_1.py --features long-log-mel --model overdrive --save overdrive_long_pl2 --pseudo_labels overdrive_long_pl1 --train --no-val
+python tom_train_1.py --features log-mel --model ap_overdrive --save ap_overdrive --train --no-val
+python tom_train_1.py --features log-mel --model ap_overdrive --save ap_overdrive_pl1 --pseudo_labels ap_overdrive --train --no-val
+python tom_train_1.py --features log-mel --model ap_overdrive --save ap_overdrive_pl2 --pseudo_labels ap_overdrive_pl1 --train --no-val
 
