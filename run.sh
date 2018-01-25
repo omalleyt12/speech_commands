1121b9c45653dde0642f9108089d0caf3fcdfa3d
Tom <omalleyt@bu.edu>
2018-01-25 01:17:23 +0000

    creating commands

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 python train.py --features log-mel --model overdrive --save overdrive --train --no-val
+python train.py --features log-mel --model overdrive --save overdrive --train --no-val
+python train.py --features log-mel --model overdrive --save overdrive --train --no-val
+python train.py --features log-mel --model overdrive --save overdrive --train --no-val
+python train.py --features log-mel --model overdrive --save overdrive --train --no-val
 
