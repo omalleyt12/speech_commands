814ff6b7a7132ef5eb0717bd7588a1ccee07670a
Tom <omalleyt@bu.edu>
2018-01-25 00:32:24 +0000

    further cleaning

--- run1.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python tom_train_1.py --features log-mel --model small_overdrive --save pi_new_small_overdrive --train --no-val --pseudo_labels overdrive
-python tom_train_1.py --features log-mel --model newdrive --save pi_new_newdrive --train --no-val --pseudo_labels overdrive
-
-
-
