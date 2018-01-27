e54f9de64e748524d8c21bf81d36abba91615a51
Tom <omalleyt@bu.edu>
2018-01-27 05:06:47 +0000

    Ready for submission

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# speech_commands
-kaggle speech commands competition
+# Instructions for creating submissions file:
+
+The commands to generate the submission file are contained in run.sh in the zip file provided. This guide assumes that the training files have been placed in a directory called “train” under this directory and that the test files are in a directory called “test”.
+The commands (contained in run.sh) are also listed below:
+
+````
+#!/usr/bin/env bash
+python train.py --features log-mel --model overdrive --save overdrive --train --no-val
+python train.py --features log-mel --model overdrive --save overdrive_pl1 --pseudo_labels overdrive --train --no-val
+python train.py --features log-mel --model overdrive --save overdrive_pl2 --pseudo_labels overdrive_pl1 --train --no-val
+python train.py --features equal-log-mel --model overdrive --save overdrive_frame_eq --train --no-val --pseudo_labels overdrive
+python train.py --features log-mel-40 --model okconv --save okconv --train --no-val
+python train.py --features mfcc-13 --model mfccnet --save mfccnet --train --no-val
+python train.py --features mfcc-13 --model mfccnet --save mfccnet_pl1 --train --no-val --pseudo_labels mfccnet
+python train.py --features short-log-mel --model overdrive --save overdrive_short --train --no-val
+python train.py --features short-log-mel --model overdrive --save overdrive_short_pl1 --pseudo_labels overdrive_short --train --no-val
+python train.py --features short-log-mel --model overdrive --save overdrive_short_pl2 --pseudo_labels overdrive_short_pl1 --train --no-val
+
+cp models/overdrive_comp.pickle ensemble_models/
+cp models/overdrive_pl1_comp.pickle ensemble_models/
+cp models/overdrive_pl2_comp.pickle ensemble_models/
+cp models/overdrive_frame_eq_comp.pickle ensemble_models/
+cp models/okconv_comp.pickle ensemble_models/
+cp models/overdrive_short_comp.pickle ensemble_models/
+cp models/overdrive_short_pl1_comp.pickle ensemble_models/
+cp models/overdrive_short_pl2_comp.pickle ensemble_models/
+
+python ensemble_sqrt.py
+````
+
--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-python tom_train_1.py --features log-mel --model overdrive --save overdrive --train --no-val
-python tom_train_1.py --features log-mel --model overdrive --save overdrive_pl1 --pseudo_labels overdrive --train --no-val
-python tom_train_1.py --features log-mel --model overdrive --save overdrive_pl2 --pseudo_labels overdrive_pl1 --train --no-val
-python tom_train_1.py --features equal-log-mel --model overdrive --save overdrive_frame_eq --train --no-val --pseudo_labels overdrive
-python tom_train_1.py --features log-mel-40 --model okconv --save okconv --train --no-val
-python tom_train_1.py --features mfcc-13 --model mfccnet --save mfccnet --train --no-val
-python tom_train_1.py --features mfcc-13 --model mfccnet --save mfccnet_pl1 --train --no-val --pseudo_labels mfccnet
-python tom_train_1.py --features short-log-mel --model overdrive --save overdrive_short --train --no-val
-python tom_train_1.py --features short-log-mel --model overdrive --save overdrive_short_pl1 --pseudo_labels overdrive_short --train --no-val
-python tom_train_1.py --features short-log-mel --model overdrive --save overdrive_short_pl2 --pseudo_labels overdrive_short_pl1 --train --no-val
+python train.py --features log-mel --model overdrive --save overdrive --train --no-val
+python train.py --features log-mel --model overdrive --save overdrive_pl1 --pseudo_labels overdrive --train --no-val
+python train.py --features log-mel --model overdrive --save overdrive_pl2 --pseudo_labels overdrive_pl1 --train --no-val
+python train.py --features equal-log-mel --model overdrive --save overdrive_frame_eq --train --no-val --pseudo_labels overdrive
+python train.py --features log-mel-40 --model okconv --save okconv --train --no-val
+python train.py --features mfcc-13 --model mfccnet --save mfccnet --train --no-val
+python train.py --features mfcc-13 --model mfccnet --save mfccnet_pl1 --train --no-val --pseudo_labels mfccnet
+python train.py --features short-log-mel --model overdrive --save overdrive_short --train --no-val
+python train.py --features short-log-mel --model overdrive --save overdrive_short_pl1 --pseudo_labels overdrive_short --train --no-val
+python train.py --features short-log-mel --model overdrive --save overdrive_short_pl2 --pseudo_labels overdrive_short_pl1 --train --no-val
 
 cp models/overdrive_comp.pickle ensemble_models/
 cp models/overdrive_pl1_comp.pickle ensemble_models/
